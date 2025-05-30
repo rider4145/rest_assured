0295fa7b11265391c2e1e22536dc98d279b36167
Rahul <rahuln14134@gmail.com>
2025-05-30 09:07:19 +0530

    rest assured 1st commit

---
+++ rest_assured/src/test/java/learning/rest_assured/http_requests.java
@@ -0,0 +1,70 @@
+package learning.rest_assured;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+/*
+ * given() -> pre-requsitives has to be defined in given() method
+				content type, set cookies, add auth, add param, set headers info etc..
+ * when() -> request type has to be defined in when() method
+ 				get, post, put, delete
+ * then() -> validation has to be defined in then() method
+ 				validate status code, extract response, extract header cookies and response body etc..
+*/
+public class http_requests {
+
+	/* 
+	 	Get: https://reqres.in/api/users?page=2
+	 	Post: https://reqres.in/api/users
+	 	 	 Body: {
+					    "name": "morpheus",
+					    "job": "leader"
+					}
+		Put: https://reqres.in/api/users/2
+			 Body: {
+				    	"name": "morpheus",
+				    	"job": "zion resident"
+					}
+		Delete: https://reqres.in/api/users/2			 
+	*/
+	@Test
+	public void getuser()
+	{
+		
+		given()
+		
+		.when()
+			.get("https://reqres.in/api/users?page=2")			// pass the get request url
+			
+		.then()
+			.statusCode(200)
+			.body("page",equalTo(2))
+			.log().all();
+	}
+	
+	@Test
+	public void createuser()
+	{
+		
+		HashMap<String, String> map= new HashMap<String,String>();
+		map.put("name", "Rahul");										// body params are defined using hashmap
+		map.put("job", "Rider");
+		
+		given()
+			.contentType("application/json")							// content.type has to be defined
+			.body(map)													// hashmap object is passed inside body values
+			 
+		.when()
+			.post("https://reqres.in/api/users")			// pass the post request url
+			
+		.then()
+			.statusCode(201)
+//			.body("name",equalTo("Rahul"))
+			.log().all();
+	}
+	
+}
